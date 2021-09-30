36b632ec9d24015ccc34787509af9aed7c121b24
Hady <hady.etd@gmail.com>
2021-09-30 01:11:39 -0400

    add bootsrap

---
+++ css/js/scripts.js
@@ -0,0 +1,6 @@
+document.querySelector(".navbar-toggler").onclick = function(){
+    setTimeout(function(){
+        let padding = document.querySelector("nav").offsetHeight + "px";
+        document.body.style.paddingTop=padding;
+    }, 500)
+};
